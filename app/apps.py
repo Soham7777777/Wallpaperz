eb888b3b7f03f7fec600ef055d5a5d6c25938892
soham <sohamjobanputra7@gmail.com>
2025-04-28 21:26:16 +0530

    MIGRATION: Created Wallpaper model with image validation, upload path generator and connected signals to auto-delete files on model update/delete

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,24 @@
 from django.apps import AppConfig as ApplicationConfig
+from django.db.models.signals import post_delete, pre_save
 
 
 class AppConfig(ApplicationConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app'
+
+
+    def ready(self) -> None:
+        from common.signals import DeleteAssociatedFilesOnModelDelete, DeleteAssociatedOldFilesOnModelUpdate
+        from app.models import Wallpaper
+
+        post_delete.connect(
+            DeleteAssociatedFilesOnModelDelete[Wallpaper](('image',)),
+            sender='app.Wallpaper',
+            dispatch_uid='WALLPAPER_DELETE_FILES_POST_DELETE'
+        )
+
+        pre_save.connect(
+            DeleteAssociatedOldFilesOnModelUpdate[Wallpaper](('image',)),
+            sender='app.Wallpaper',
+            dispatch_uid='WALLPAPER_DELETE_OLD_FILES_PRE_SAVE'
+        )
--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
+from django.db import models
+from pathlib import PurePath
 from common.models import AbstractBaseModel
+from common.image_utils import ImageFormat
+from common.validators import ImageFormatAndFileExtensionsValidator, MaxFileSizeValidator
+from common.unique_file_path_generators import UniqueFilePathGenerator
+from common.constants import MB
 
-# Create your models here.
+
+class Wallpaper(AbstractBaseModel):
+    image = models.ImageField(
+        unique=True,
+        upload_to=UniqueFilePathGenerator(
+            PurePath('wallpapers/'),
+            'image'
+        ),
+        validators=[
+            MaxFileSizeValidator(7 * MB),
+            ImageFormatAndFileExtensionsValidator((ImageFormat.JPEG, ))
+        ],
+        max_length=256
+    )
+
+    objects: models.Manager["Wallpaper"] = models.Manager()
