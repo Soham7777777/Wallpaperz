e1c1c1c25beeb89a8fd58ae11404cea72278c4d8
soham <sohamjobanputra7@gmail.com>
2025-04-29 12:37:37 +0530

    homepage done

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,34 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from app.models import Wallpaper
 
 
 urlpatterns = [
 
-    path('', TemplateView.as_view(template_name='app/home.html'), name='home'),
-    path('categories', TemplateView.as_view(template_name='app/categories.html'), name='categories'),
+    path(
+        '', 
+        TemplateView.as_view(
+            template_name='app/home/page.html'
+        ), 
+        name='home'
+    ),
+
+    path(
+        'wallpapers',
+        ListView.as_view(
+            model=Wallpaper,
+            paginate_by=9, 
+            template_name='app/home/ajax/wallpapers.html'
+        ), 
+        name='wallpapers'
+    ),
+
+    path(
+        'categories', 
+        TemplateView.as_view(
+            template_name='app/categories.html'
+        ), 
+        name='categories'
+    ),
 
 ]
